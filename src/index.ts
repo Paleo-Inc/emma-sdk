9bfc70b975f809e364a9509ad07665178c2595d6
Sarthak Kaushik <skkaushik212@gmail.com>
2023-06-01 14:38:22 +0530

    shopify integration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { AuthenticationEnum } from "./enum";
-import { AuthenticationType } from "./types";
+import { fetcher, HttpMethod } from "./fetcher";
+import { AuthenticationType, InputType, executeFunctionType } from "./types";
 
 export default class EmmaSdk {
   authenticationType: AuthenticationEnum;
   authorizationUrl?: string | null;
   tokenUrl?: string | null;
   scope?: string[] | null;
+  userInput?: InputType[];
+  [key: string]: any;
 
   constructor() {
     this.authenticationType = AuthenticationEnum.HEADERBEARER;
@@ -23,9 +26,60 @@ export default class EmmaSdk {
     }
   }
 
-  getauthenticationType() {
-    return this.authenticationType;
+  setInputType(inputs: InputType[]) {
+    for (let i = 0; i < inputs.length; i++) {
+      this[inputs[i].key] = "";
+    }
+    this.userInput = inputs;
+  }
+
+  setInputValue(key: string, value: string) {
+    this[key] = value;
+  }
+
+  getInputValues() {
+    let inputValues: any = {};
+    for (let key in this) {
+      if (typeof this[key] !== "function") {
+        inputValues[key] = this[key];
+      }
+    }
+    return inputValues;
+  }
+
+  addExecution(execute: executeFunctionType) {
+    let inputValues: any = {};
+    let isInputEmpty = false;
+    for (let key in this.userInput) {
+      if (this[key] !== "") {
+        inputValues[key] = this[key];
+      } else {
+        isInputEmpty = true;
+        break;
+      }
+    }
+    if (isInputEmpty) {
+      return {
+        status: false,
+        message: "User input are empty",
+      };
+    }
+    this.executionFunction = execute;
+  }
+
+  async runFunction() {
+    if (this.executionFunction) {
+      let inputValues: any = {};
+      for (let key in this.userInput) {
+        if (this[key] !== "") {
+          inputValues[key] = this[key];
+        }
+      }
+      const returnValue = await this.executionFunction(fetcher);
+      return returnValue;
+    }
+    return "No Values";
   }
 }
 
-export { AuthenticationEnum };
+export { AuthenticationEnum, HttpMethod };
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,21 @@
 import { AuthenticationEnum } from "./enum";
+import { AxiosFetcherOptions, AxiosFetcherResponse } from "./fetcher";
 
 export type AuthenticationType = {
   type: AuthenticationEnum;
   authorizationUrl?: string;
   tokenUrl?: string;
   scope?: string[];
+  customKey?: string;
 };
+
+export type InputType = {
+  key: string;
+  type: string;
+};
+
+export type AxiosFetcherFunction<T> = (
+  options: AxiosFetcherOptions<T>
+) => Promise<AxiosFetcherResponse<T>>;
+
+export type executeFunctionType = (fetcher: AxiosFetcherFunction<any>) => void;
